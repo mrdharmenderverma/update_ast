131c0abec21c8a2c06cc8a507fb74df82ffd4178
mrdharmenderverma <dharmenderkumar223@gmail.com>
2023-10-31 18:29:33 +0530

    update services section

--- index.php
+++ index.php
@@ -100,8 +100,249 @@
     </section>
     <!---About End--->
 
+    <section class="about-section bg_light_1">
+        <!--===============spacing==============-->
+        <div class="pd_bottom_80"></div>
+        <!--===============spacing==============-->
+        <div class="container">
+            <!--===============spacing==============-->
+            <div class="pd_bottom_40"></div>
+            <!--===============spacing==============-->
+            <div class="row mb-4">
+                <div class="col-lg-12">
+                    <div class="title_all_box style_two text-center dark_color">
+                        <div class="title_sections two">
+                            <!-- <div class="before_title"> Effective Solutions</div> -->
+                            <h2 class=" main-heading-stroke homeservices-stroke">Services We Offer</h2>
+                        </div>
+                    </div>
+                </div>
+                <!--===============spacing==============-->
+                <div class="mr_bottom_10"></div>
+                <!--===============spacing==============-->
+            </div>
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-12 mb-4">
+                    <div class="row border rounded-3 p-2">
+                        <div class="col-lg-9 col-md-9 col-sm-12">
+                            <div class="row gutter_15px my-3">
+                                <div class="col-lg-12">
+                                    <div class="icon_box_all  style_two">
+                                        <div class="icon_content icon_imgs ">
+                                            <div class="">
+                                                <!-- <img src="assets/images/icon-img-n-1.png" class="img-fluid svg_image" alt="icon png"> -->
+                                                <img src="assets/images/home/Software.svg" alt="" srcset="" class="img-fluid svg_image">
+                                            </div>
+                                            <div class="txt_content">
+                                                <h3>Software Development</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <p>We are India's leading software development company since 2016. Our skilled professionals can provide scalable, secure, and reliable software solutions. We deliver the best custom solutions with recent IT advancements...</p>
+                            <a href="software-develpment.php" rel="nofollow" class="theme-btn five mb-3">
+                                View More<i class="icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 rounded-3 responsive-services border text-center" style="
+                                    padding-top: 40px;
+                                    background: var(--primary-color-four);">
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/C Language.svg" alt="C Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C Language</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Python.svg" alt="Python" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Python</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Java.svg" alt="Java" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Java</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/c.svg" alt="Go Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C#</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-12 mb-lg-0 mb-xl-0 mb-2">
+                    <div class="row border rounded-3 p-2">
+                        <div class="col-lg-9 col-md-9 col-sm-12">
+                            <div class="row gutter_15px my-3">
+                                <div class="col-lg-12">
+                                    <div class="icon_box_all  style_two">
+                                        <div class="icon_content icon_imgs ">
+                                            <div class="">
+                                                <!-- <img src="assets/images/icon-img-n-1.png" class="img-fluid svg_image" alt="icon png"> -->
+                                                <img src="assets/images/home/Software.svg" alt="" srcset="" class="img-fluid svg_image">
+                                            </div>
+                                            <div class="txt_content">
+                                                <h3>Software Development</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <p>We are India's leading software development company since 2016. Our skilled professionals can provide scalable, secure, and reliable software solutions. We deliver the best custom solutions with recent IT advancements...</p>
+                            <a href="software-develpment.php" rel="nofollow" class="theme-btn five mb-3">
+                                View More<i class="icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 rounded-3 responsive-services border py-4 text-center" style="
+                                    
+                                    background: var(--primary-color-four);">
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/C Language.svg" alt="C Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C Language</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Python.svg" alt="Python" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Python</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Java.svg" alt="Java" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Java</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/c.svg" alt="Go Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C#</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+                <div class="col-xl-6 col-lg-6 col-md-12 mb-lg-0 mb-xl-0 mb-2">
+                    <div class="row border rounded-3 p-2">
+                        <div class="col-lg-9 col-md-9 col-sm-12">
+                            <div class="row gutter_15px my-3">
+                                <div class="col-lg-12">
+                                    <div class="icon_box_all  style_two">
+                                        <div class="icon_content icon_imgs ">
+                                            <div class="">
+                                                <!-- <img src="assets/images/icon-img-n-1.png" class="img-fluid svg_image" alt="icon png"> -->
+                                                <img src="assets/images/home/Software.svg" alt="" srcset="" class="img-fluid svg_image">
+                                            </div>
+                                            <div class="txt_content">
+                                                <h3>Software Development</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <p>We are India's leading software development company since 2016. Our skilled professionals can provide scalable, secure, and reliable software solutions. We deliver the best custom solutions with recent IT advancements...</p>
+                            <a href="software-develpment.php" rel="nofollow" class="theme-btn five mb-3">
+                                View More<i class="icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 rounded-3 responsive-services border py-4 text-center" style="
+                                    
+                                    background: var(--primary-color-four);">
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/C Language.svg" alt="C Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C Language</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Python.svg" alt="Python" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Python</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Java.svg" alt="Java" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Java</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/c.svg" alt="Go Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C#</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+                <div class="col-xl-6 col-lg-6 col-md-12 mb-lg-0 mb-xl-0 mb-2">
+                    <div class="row border rounded-3 p-2">
+                        <div class="col-lg-9 col-md-9 col-sm-12">
+                            <div class="row gutter_15px my-3">
+                                <div class="col-lg-12">
+                                    <div class="icon_box_all  style_two">
+                                        <div class="icon_content icon_imgs ">
+                                            <div class="">
+                                                <!-- <img src="assets/images/icon-img-n-1.png" class="img-fluid svg_image" alt="icon png"> -->
+                                                <img src="assets/images/home/Software.svg" alt="" srcset="" class="img-fluid svg_image">
+                                            </div>
+                                            <div class="txt_content">
+                                                <h3>Software Development</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <p>We are India's leading software development company since 2016. Our skilled professionals can provide scalable, secure, and reliable software solutions. We deliver the best custom solutions with recent IT advancements...</p>
+                            <a href="software-develpment.php" rel="nofollow" class="theme-btn five mb-3">
+                                View More<i class="icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 rounded-3 responsive-services border py-4 text-center" style="
+                                    
+                                    background: var(--primary-color-four);">
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/C Language.svg" alt="C Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C Language</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Python.svg" alt="Python" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Python</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Java.svg" alt="Java" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Java</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/c.svg" alt="Go Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C#</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+        </div>
+        <!--===============spacing==============-->
+        <div class="pd_bottom_80"></div>
+        <!--===============spacing==============-->
+    </section>
+
     <!---Service Start--->
-    <section class="service-section bg_light_1 py-5">
+    <section class="service-section  py-5">
         <div class="container">
             <div class="row mb-4">
                 <div class="col-lg-12">
---
+++ test.php
@@ -0,0 +1,143 @@
+<!-- Header File -->
+<?php $current_page_url = "https://avtarspace.com/policy";
+$title = 'AST | Test';
+include_once 'header.php'; ?>
+<!-- Header File -->
+<style>
+    @media(max-width:767px) {
+        .responsive-services {
+            display: flex;
+            justify-content: space-around;
+        }
+    }
+</style>
+<!--===============PAGE CONTENT ==============-->
+<div id="content" class="site-content">
+    <section class="about-section">
+        <!--===============spacing==============-->
+        <div class="pd_bottom_80"></div>
+        <!--===============spacing==============-->
+        <div class="container">
+            <!--===============spacing==============-->
+            <div class="pd_bottom_40"></div>
+            <!--===============spacing==============-->
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-12 mb-lg-0 mb-xl-0 mb-2">
+                    <div class="row border rounded-3 p-2">
+                        <div class="col-lg-9 col-md-9 col-sm-12">
+                            <div class="row gutter_15px my-3">
+                                <div class="col-lg-12">
+                                    <div class="icon_box_all  style_two">
+                                        <div class="icon_content icon_imgs ">
+                                            <div class="">
+                                                <!-- <img src="assets/images/icon-img-n-1.png" class="img-fluid svg_image" alt="icon png"> -->
+                                                <img src="assets/images/home/Software.svg" alt="" srcset="" class="img-fluid svg_image">
+                                            </div>
+                                            <div class="txt_content">
+                                                <h3>Software Development</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <p>We are India's leading software development company since 2016. Our skilled professionals can provide scalable, secure, and reliable software solutions. We deliver the best custom solutions with recent IT advancements...</p>
+                            <a href="software-develpment.php" rel="nofollow" class="theme-btn five mb-3">
+                                View More<i class="icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 rounded-3 responsive-services border text-center" style="
+                                    background: var(--primary-color-four);">
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/C Language.svg" alt="C Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C Language</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Python.svg" alt="Python" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Python</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Java.svg" alt="Java" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Java</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/c.svg" alt="Go Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C#</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-12 mb-lg-0 mb-xl-0 mb-2">
+                    <div class="row border rounded-3 p-2">
+                        <div class="col-lg-9 col-md-9 col-sm-12">
+                            <div class="row gutter_15px my-3">
+                                <div class="col-lg-12">
+                                    <div class="icon_box_all  style_two">
+                                        <div class="icon_content icon_imgs ">
+                                            <div class="">
+                                                <!-- <img src="assets/images/icon-img-n-1.png" class="img-fluid svg_image" alt="icon png"> -->
+                                                <img src="assets/images/home/Software.svg" alt="" srcset="" class="img-fluid svg_image">
+                                            </div>
+                                            <div class="txt_content">
+                                                <h3>Software Development</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <p>We are India's leading software development company since 2016. Our skilled professionals can provide scalable, secure, and reliable software solutions. We deliver the best custom solutions with recent IT advancements...</p>
+                            <a href="software-develpment.php" rel="nofollow" class="theme-btn five mb-3">
+                                View More<i class="icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 rounded-3 responsive-services border py-4 text-center" style="
+                                    
+                                    background: var(--primary-color-four);">
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/C Language.svg" alt="C Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C Language</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Python.svg" alt="Python" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Python</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/Java.svg" alt="Java" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">Java</a>
+                                </p>
+                            </div>
+                            <div class="text_box_inner">
+                                <img src="assets/images/home/c.svg" alt="Go Language" srcset="" style="max-width: 31.67px; height: auto">
+                                <p class="card-icon-name" style="font-size: 10px">
+                                    <a href="#" rel="bookmark">C#</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <!--===============spacing==============-->
+        <div class="pd_bottom_80"></div>
+        <!--===============spacing==============-->
+    </section>
+</div>
+<!--===============PAGE CONTENT END==============-->
+</div>
+
+<!-- Footer  Start -->
+<?php include_once 'footer.php'; ?>
+<!-- Footer  End -->
